e70396b745883e8f57dd4a30ff8af89a34ecdd33
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2024-08-18 15:56:37 +0200

    initial version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MatrixViewComponent } from './matrix-view/matrix-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { title: 'matrix', path: 'matrix', component: MatrixViewComponent },
+];
